e3f70704543d653edb6b69b29e467899ba903ea0
Arul1541 <arulkumarans2022@gmail.com>
2021-04-30 20:35:38 +0530

    firststep

--- djangofirst/firstapp/views.py
+++ djangofirst/firstapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+import datetime
 # Create your views here.
+
+def greet(request):
+    msg='Good morning'
+    return HttpResponse(msg)
+def time(request):
+    t=datetime.datetime.now()
+    msg='The TIME is :'+str(t)
+    return HttpResponse(msg)
